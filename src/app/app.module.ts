94f8cb8793dcea9b4501f30de26babf3de134042
Tarek Yaghi <you@example.com>
2024-04-13 12:44:40 +0100

    first

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainMenuComponent } from './header/main-menu/main-menu.component';
+import { TopMenuComponent } from './header/top-menu/top-menu.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { SearchComponent } from './product-list/search/search.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MainMenuComponent,
+    TopMenuComponent,
+    ProductListComponent,
+    SearchComponent,
+    TopHeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
