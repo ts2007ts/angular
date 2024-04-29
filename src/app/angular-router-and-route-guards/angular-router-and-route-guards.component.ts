a06398a6ed3ecc814571bf7fb346e5cd6d93700e
Tarek Yaghi <you@example.com>
2024-04-29 17:56:46 +0100

    1

--- src/app/angular-router-and-route-guards/angular-router-and-route-guards.component.ts
+++ src/app/angular-router-and-route-guards/angular-router-and-route-guards.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, inject } from '@angular/core';
+import { Event, NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-angular-router-and-route-guards',
@@ -6,6 +7,22 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrl: './angular-router-and-route-guards.component.css',
   encapsulation: ViewEncapsulation.ShadowDom
 })
-export class AngularRouterAndRouteGuardsComponent {
+export class AngularRouterAndRouteGuardsComponent implements OnInit {
 
+  showLoader: boolean = false;
+  router: Router = inject(Router);
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationStart) {
+        this.showLoader = true;
+      } else if (
+        event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError
+      ) {
+        this.showLoader = false;
+      }
+    })
+  }
 }
--- src/app/angular-router-and-route-guards/checkout-router/checkout-router.component.ts
+++ src/app/angular-router-and-route-guards/checkout-router/checkout-router.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Course } from '../Models/course';
 
 @Component({
   selector: 'app-checkout-router',
   templateUrl: './checkout-router.component.html',
   styleUrl: './checkout-router.component.css'
 })
-export class CheckoutRouterComponent {
+export class CheckoutRouterComponent implements OnInit {
 
+  activeRoute = inject(ActivatedRoute);
+  router = inject(Router);
+  course: any;
+
+  ngOnInit(): void {
+    // this.activeRoute.data.subscribe((data) => {
+    //   this.course = data;
+    // })
+
+    //this.course = this.router.getCurrentNavigation().extras.state;
+    this.course = history.state;
+  }
 }
